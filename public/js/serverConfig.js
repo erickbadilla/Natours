dd601fbaca3b8a5e656a2e981f59bd848132f0cb
Erick <rvoltpwn@gmail.com>
2022-10-23 10:48:51 -0600

    Little fix

--- public/js/serverConfig.js
+++ public/js/serverConfig.js
@@ -5,6 +5,6 @@ export const serverUrl = 'http://127.0.0.1';
 export const serverPort = '8080';
 export const severAPIV1Path = 'api/v1';
 export const serverAPIRouteV1 =
-  window.location.protocol === 'https'
+  window.location.protocol === 'https:'
     ? severAPIV1Path
     : `${serverUrl}:${serverPort}/${severAPIV1Path}`;
